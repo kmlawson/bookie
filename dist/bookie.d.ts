31edd0ae27bb2c719aa7626c4de3eab3e895010d
Glenn F. Henriksen <gh@justify.no>
2021-08-16 08:46:32 +0200

    Card layout and fixes URL -> url

--- dist/bookie.d.ts
+++ dist/bookie.d.ts
@@ -4,22 +4,38 @@ interface Source {
         family: string;
         given: string;
         droppingParticle: string;
+        literal: string;
     }[];
     issued: {
         dateParts: (string | number)[][];
     };
-    URL: string;
+    url: string;
     tags: {
         tag: string;
         type: string | null;
     }[];
 }
+declare class Book {
+    private source;
+    private typelessTags;
+    private authorList;
+    constructor(source: Source);
+    get title(): string;
+    get issued(): string;
+    get tags(): string[];
+    get authors(): string[];
+    get url(): string;
+}
 export declare class Bookie {
-    sources: Source[] | null;
+    sources: Book[] | null;
     tagElement: HTMLElement | null;
     resultsElement: HTMLElement | null;
-    init(elementId: string, filepath: string): Promise<void>;
+    init(elementId: string, filepath: string): void;
     private show;
+    private appendTags;
+    private appendIssued;
+    private appendTitle;
+    private appendAuthors;
     private request;
 }
 export {};
--- src/bookie.ts
+++ src/bookie.ts
@@ -1,64 +1,129 @@
 import camelcaseKeys from 'camelcase-keys';
-import { SourceCode } from 'eslint';
+
 interface Source {
   title: string;
   author: {
     family: string;
     given: string;
     droppingParticle: string;
+    literal: string;
   }[];
   issued: {
     dateParts: (string | number)[][];
   };
-  URL: string;
+  url: string;
   tags: {
     tag: string;
     type: string | null;
   }[];
 }
 
-/* eslint-disable-next-line @typescript-eslint/no-unused-vars */ //  Class is only used externally
+class Book {
+  private source: Source;
+  private typelessTags: string[];
+  private authorList: string[];
+  constructor(source: Source) {
+    this.source = source;
+    this.typelessTags = source.tags.filter((t) => !t.type).map((t) => t.tag);
+    if (source.author) {
+      this.authorList = source.author.map((a) => {
+        if (!a) return '';
+        if (a.literal) return a.literal;
+        return (
+          (a.droppingParticle ? a.droppingParticle + ' ' : '') +
+          a.family +
+          (a.given ? ', ' + a.given : '')
+        );
+      });
+    } else {
+      this.authorList = [''];
+    }
+  }
+
+  get title(): string {
+    return this.source.title;
+  }
+  get issued(): string {
+    if (this.source.issued?.dateParts) {
+      return '(' + this.source.issued.dateParts[0][0] + ')';
+    }
+    return '';
+  }
+  get tags(): string[] {
+    return this.typelessTags;
+  }
+  get authors(): string[] {
+    return this.authorList;
+  }
+  get url(): string {
+    return this.source.url;
+  }
+}
+
 export class Bookie {
-  sources: Source[] | null = null;
+  sources: Book[] | null = null;
   tagElement: HTMLElement | null = null;
   resultsElement: HTMLElement | null = null;
 
-  async init(elementId: string, filepath: string) {
+  init(elementId: string, filepath: string): void {
     const elem = document.getElementById(elementId);
     if (elem === null) {
       return;
     }
-    this.sources = await this.request<Source[]>(filepath);
-
-    elem.innerHTML = '';
-    elem.className = 'dsf';
-    this.tagElement = document.createElement('div');
-    this.tagElement.className = 'bookie__tags';
-    elem.appendChild(this.tagElement);
-    this.resultsElement = document.createElement('div');
-    this.resultsElement.className = 'bookie__result';
-    elem.appendChild(this.resultsElement);
-
-    const tagSet = new Set<string>();
-    this.sources.forEach((source) => {
-      if (source.tags != null) {
-        source.tags.forEach((tag) => {
-          if (tag.type === undefined) {
-            tagSet.add(tag.tag);
-          }
-        });
+    this.request<Source[]>(filepath).then((sources: Source[]) => {
+      if (!sources) return;
+      this.sources = sources
+        .map((s) => new Book(s))
+        .sort((a, b) =>
+          a.authors[0]
+            .toUpperCase()
+            .localeCompare(b.authors[0].toUpperCase()) !== 0
+            ? a.authors[0]
+                .toUpperCase()
+                .localeCompare(b.authors[0].toUpperCase())
+            : a.title.toUpperCase().localeCompare(b.title.toUpperCase()),
+        );
+      const firstBookWithAuthor = this.sources.find((b) => b.authors[0] !== '');
+      if (firstBookWithAuthor !== undefined) {
+        const indexOfFirstBookWithAuthors =
+          this.sources.indexOf(firstBookWithAuthor);
+        const noAuthors = this.sources.slice(0, indexOfFirstBookWithAuthors);
+        const hasAuthors = this.sources.slice(
+          indexOfFirstBookWithAuthors,
+          this.sources.length,
+        );
+        this.sources = [...hasAuthors, ...noAuthors];
       }
-    });
 
-    tagSet.forEach((tag) => {
-      if (this.tagElement === null) return;
-      const button = document.createElement('button');
-      button.textContent = tag;
-      button.className = 'bookie__tag';
-      button.onclick = () => {
-        this.show(tag);
-      };
-      this.tagElement.appendChild(button);
+      elem.innerHTML = '';
+      elem.className = '';
+
+      this.tagElement = document.createElement('div');
+      this.tagElement.className = 'bookie__tags';
+      elem.appendChild(this.tagElement);
+      this.resultsElement = document.createElement('div');
+      this.resultsElement.className = 'bookie__result';
+      elem.appendChild(this.resultsElement);
+
+      const tagSet = new Set<string>();
+      this.sources.forEach((source) => {
+        if (source.tags != null) {
+          source.tags.forEach((tag) => {
+            tagSet.add(tag);
+          });
+        }
+      });
+
+      tagSet.forEach((tag) => {
+        if (this.tagElement === null) return;
+        const button = document.createElement('button');
+        button.textContent = tag;
+        button.className = 'bookie__tag';
+        button.onclick = () => {
+          this.show(tag);
+        };
+        this.tagElement.appendChild(button);
+      });
     });
   }
 
@@ -72,46 +137,61 @@ export class Bookie {
 
     this.resultsElement.appendChild(list);
 
-    const taggedSources = this.sources.filter(
-      (source) =>
-        source.tags?.find((t) => t.type === undefined && t.tag === tag) !==
-        undefined,
+    const taggedSources = this.sources.filter((source) =>
+      source.tags?.find((t) => t === tag),
     );
 
     taggedSources.forEach((source) => {
       const item = document.createElement('li');
-      console.log(source);
-      if (source.author) {
-        const authorData = source.author.find((a) => a.family);
-        if (authorData) {
-          const author = document.createElement('span');
-          author.textContent =
-            (authorData.droppingParticle
-              ? authorData.droppingParticle + ' '
-              : '') +
-            authorData.family +
-            (authorData.given ? ', ' + authorData.given : '') +
-            '. ';
-          item.appendChild(author);
-        }
-      }
-
-      const title = document.createElement('span');
-      const titleLink = document.createElement('a') as HTMLAnchorElement;
-      titleLink.href = source.URL;
-      titleLink.innerText = source.title;
-      title.appendChild(titleLink);
-      item.appendChild(title);
       item.className = 'bookie__result__item';
 
-      if (source.issued?.dateParts) {
-        const issued = document.createElement('span');
-        issued.textContent = ' (' + source.issued.dateParts[0][0] + ')';
-        item.appendChild(issued);
-      }
+      this.appendAuthors(source, item);
+      this.appendTitle(source, item);
+      this.appendIssued(source, item);
+      this.appendTags(source, item);
 
       list.appendChild(item);
     });
+  }
+
+  private appendTags(source: Book, item: HTMLLIElement) {
+    const tags = document.createElement('div');
+    tags.className = 'bookie__result__item__tags';
+    source.tags.forEach((t) => {
+      const tag = document.createElement('span');
+      tag.textContent = t;
+      tags.appendChild(tag);
+    });
+    item.appendChild(tags);
+  }
+
+  private appendIssued(source: Book, item: HTMLLIElement) {
+    const issued = document.createElement('span');
+    issued.textContent = ' ' + source.issued;
+    item.appendChild(issued);
+  }
+
+  private appendTitle(source: Book, item: HTMLLIElement) {
+    const title = document.createElement('span');
+    title.className = 'bookie__result__item__title';
+    if (source.url) {
+      const titleLink = document.createElement<'a'>('a');
+      titleLink.href = source.url;
+      titleLink.innerText = source.title;
+      title.appendChild(titleLink);
+    } else {
+      title.textContent = source.title;
+    }
+    item.appendChild(title);
+  }
+
+  private appendAuthors(source: Book, item: HTMLLIElement) {
+    const author = document.createElement('span');
+    author.className = 'bookie__result__item__authors';
+    author.innerHTML = source.authors
+      .map((a) => '<div>' + a + '</div>')
+      .join('');
+    item.appendChild(author);
   }
 
   private async request<TResponse>(
